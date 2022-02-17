181d6fce06f953827973a7b9807375b5cf1ac4f0
styopochkin23 <92596661+styopochkin23@users.noreply.github.com>
2022-02-17 18:19:05 +0200

    labRob_2
    labRob_2

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int test = 20;
+    int test2 = 10;
+    System.out.println(test % test2);
+        System.out.println(test / test2);
+        System.out.println(test + test2);
+        System.out.println();
+        double test3 = test / 3;
+        System.out.println(test3);
+    }
+}
